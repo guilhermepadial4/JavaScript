fdd886f196679290230269cb653b7f7a21ef4f4a
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-06 15:07:49 -0300

    Aula012 criada

---
+++ Exercicios/aula012/aula012.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const objs1=document.getElementsByTagName("div")
+const objs2=[...document.getElementsByTagName("div")]
+
+objs2.forEach(element =>{
+    element.innerHTML="curso"
+});
+
+console.log(objs1) 
+console.log(objs2)
