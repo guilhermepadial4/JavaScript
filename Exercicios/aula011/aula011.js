a1fc893d3e1f36adcb12821199575fcbd4673d2b
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-06 12:03:06 -0300

    Aula011 criada

---
+++ Exercicios/aula011/aula011.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let v1=10
+let v2="10"
+let v3=v1===v2
+let v4={nome: "Guilherme"}
+
+console.log("Valor: " + v1 + " - Tipo: " + typeof(v1))
+console.log("Valor: " + v1 + " - Tipo: " + typeof(v2))
+console.log("Valor: " + v1 + " - Tipo: " + typeof(v3))
+console.log("Valor: " + v1 + " - Tipo: " + typeof(v4))
