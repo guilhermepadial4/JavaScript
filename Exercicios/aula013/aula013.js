0114e853ac50c69a932dc0d6607c8b010796aa4a
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-07 12:39:51 -0300

    aula 013 criada

---
+++ Exercicios/aula013/aula013.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let energia=100
+let clima="chuva"
+
+if(energia > 70 && clima=="sol"){
+    console.log("Vou a praia")
+}else{
+    console.log("Vou ao cinema")
+}
+    
+console.log("Fim do programa")
