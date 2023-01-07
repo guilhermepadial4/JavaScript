361a4d2667064e13824d2867cb414a80f6d77bb9
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-07 11:45:41 -0300

    Atualização na aula012

--- Exercicios/aula012/aula012.js
+++ Exercicios/aula012/aula012.js
@@ -3,9 +3,9 @@
 const objs1=document.getElementsByTagName("div")
 const objs2=[...document.getElementsByTagName("div")]
 
-objs2.forEach(element =>{
+objs2.forEach(element => {
     element.innerHTML="curso"
 });
 
-console.log(objs1) 
+console.log(objs1)
 console.log(objs2)
