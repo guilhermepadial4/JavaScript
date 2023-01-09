694816676030d0f0706b4a9fce323ea43c7b1226
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-09 10:59:00 -0300

    Aula015 criada

---
+++ Exercicios/aula015/aula015.js
@@ -0,0 +1,13 @@
+"use strict"
+
+console.log("Começo do programa")
+
+for(let i=0; i < 100; i++){
+    if(i%2==0){
+        console.log(i + " par")
+    }else{
+        console.log(i + " impar")
+    }
+}
+
+console.log("Fim do programa")
