97b20dbb8aeb7effb5ceb30b6421cff8017dc28d
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-10 15:10:42 -0300

    Aula020 criada

---
+++ Exercicios/aula020/aula020.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function mostrarSaldo(){
+    let d2=document.getElementById("d2")
+    d2.innerHTML="Saldo R$ 1,502,123,99"
+}
+
+function ocultarSaldo(){
+    let d2=document.getElementById("d2")
+    d2.innerHTML="Saldo R$ *****"
+}
