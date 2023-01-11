785f06f808b21a66aa8a608f216d8862952278a7
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-11 10:33:22 -0300

    Update aula021

--- Exercicios/aula021/aula021.js
+++ Exercicios/aula021/aula021.js
@@ -3,13 +3,12 @@
 function soma(){
    let n1=9
    let n2=3
-   let res=n1*n2
+   let res=n1+n2
    if(res%2 == 0){
     return "Par"
 }else{
     return "Impar"  
 }
-   return res
 }
 
 let res=soma()
