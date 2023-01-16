e0680f0bfe929e2c5a85db6c26a4145fea3f007c
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-16 09:56:40 -0300

    Update aula027

--- Exercicios/aula027/aula027.js
+++ Exercicios/aula027/aula027.js
@@ -1,16 +1,14 @@
 "use strict"
 
-function* cores(){
-    yield 'Roxo'
-    yield 'Azul'
-    yield 'Preto'
-    yield 'Cinza'
-    yield 'Amarelo'
+function* contador(){
+    let i=0
+    while(true){
+        yield i++
+        if(i>5)
+            break
+    }
 }
-
-const itc=cores()
-console.log(itc.next().value)
-console.log(itc.next().value)
-console.log(itc.next().value)
-console.log(itc.next().value)
-console.log(itc.next().value)
+const itc=contador()
+for(let c of itc){
+    console.log(c)
+}
