c862832081630eca510d162624faf13b158657c2
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-16 10:43:18 -0300

    Aula028 criada

---
+++ Exercicios/aula028/aula028.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const el=document.getElementsByTagName("div")
+el.map((e, i)=>{
+    console.log(e)
+})
+console.log(el)
+
+
+
+
+
+
+
+// const cursos=['HTML','CSS','Javascript','PHP','React']
+// let c=cursos.map((el,i)=>{
+//     return el
+// })
+
+// console.log(c)
