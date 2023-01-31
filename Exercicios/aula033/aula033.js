2a7021d368a26002492d4618871f752c5c741d88
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-01-31 15:01:35 -0300

    Update aula033

--- Exercicios/aula033/aula033.js
+++ Exercicios/aula033/aula033.js
@@ -5,7 +5,8 @@ const cursosC1 = [...document.getElementsByClassName ("c1")]
 const cursosC2 = [...document.getElementsByClassName ("c2")]
 const cursoEspecial = document.getElementById ("c1")
 
-const query_divTodas = [...document.querySelectorAll ("div[class]")]
+//const query_divTodas = [...document.querySelectorAll ("div[class]")]
+const query_divTodas = [...document.querySelectorAll ("div > p")]
 const query_cursoTodos = [...document.querySelectorAll (".curso")]
 const query_cursosC1 = [...document.querySelectorAll (".c1")]
 const query_cursosC2 = [...document.querySelectorAll (".c2")]
