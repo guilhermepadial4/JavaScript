4e11c873ecb5ff96f03374546e2658fbcf8972de
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-02-08 10:59:05 -0300

    Aula040 Criada

---
+++ Exercicios/aula040/aula040.js
@@ -0,0 +1,4 @@
+const caixa1 = document.querySelector ('#caixa1')
+const btn_c = [...document.querySelectorAll ('.curso')]
+const c1_2 = document.querySelector ('#c1_2')
+const cursos = ["HTML", "CSS", "Javascrit", "PHP", "", "", ""]
