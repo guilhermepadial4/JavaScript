bf97e7ab51a948f7444b0b056007ff6191c25d0f
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-02-13 09:23:54 -0300

    Update aula043

--- Exercicios/aula043/aula043.js
+++ Exercicios/aula043/aula043.js
@@ -2,7 +2,7 @@ const caixaCursos = document.querySelector('#caixaCursos')
 const btn_c = [...document.querySelectorAll(".curso")]
 const c1_2 = document.querySelector("#c1_2")
 const cursos = ["HTML", "CSS", "Javascript", "PHP", "React", "MySQL", "ReactNative"]
-const btnCursoSelecionado = document.getElementById ('btnCursoSelecionado')
+const btnCursoSelecionado = document.getElementById ('btnCursosSelecionados')
 
 cursos.map((el, chave)=>{
     const novoElement = document.createElement ("div")
@@ -13,7 +13,7 @@ cursos.map((el, chave)=>{
     const comandos = document.createElement('div')
     comandos.setAttribute('class', 'comandos')
 
-    const rb = document.createAttribute ('input')
+    const rb = document.createElement ('input')
     rb.setAttribute('type', 'radio')
     rb.setAttribute('name', 'rb_curso')
 
@@ -22,4 +22,21 @@ cursos.map((el, chave)=>{
     novoElement.appendChild(comandos)
 
     caixaCursos.appendChild(novoElement)
+
+})
+
+
+btnCursoSelecionado.addEventListener("click", (evt)=>{
+    const todosRadios=[...document.querySelectorAll("input[type=radio]")]
+    const radioSelecionado = todosRadios.filter ((ele)=>{
+        return ele.checked
+    })
+    
+radioSelecionado = radioSelecionado[0]
+//const cursoSelecionado = radioSelecionado.parentNode.parentNode.firstChild.textContent
+const cursoSelecionado = radioSelecionado.parentNode.previousSibling.textContent
+alert("Curso Selecionado: " + cursoSelecionado)
+
+// console.log(todosRadios)
+// console.log(radioSelecionado)
 })
