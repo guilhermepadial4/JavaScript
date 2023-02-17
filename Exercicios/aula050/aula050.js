5d090e92061e623655dc7dc1c98fc279c1039b4e
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-02-17 09:50:16 -0300

    Update aula050

--- Exercicios/aula050/aula050.js
+++ Exercicios/aula050/aula050.js
@@ -1,26 +1,2 @@
-let valores = [1,2]
-const op = [
-    (val)=>{
-        let res = 0
-        for (let v of val){
-            res += val
-            
-        }
-        return res
-    },
-    (val)=>{
-        let res = 0
-        for (let v of val){
-            res *= val
-            
-        }
-        return res
-    },
-    (val)=>{
-        for (let v of val){
-            console.log(v)
-        }
-    }
-]
 
-console.log(op)
+const btn_soma =
