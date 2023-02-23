568666fde2d12c9aa16083ce94e0090223a23836
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-02-23 10:24:36 -0300

    Update aula054

---
+++ Exercicios/aula054/aula054.js
@@ -0,0 +1,13 @@
+class Pessoa {
+    constructor(pnome){
+        this.nome = pnome
+    }
+}
+
+let p1 = new Pessoa("Guilherme")
+let p2 = new Pessoa("Rafael")
+let p3 = new Pessoa("Lorena")
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
