5756788de933b42bb4c9ec301a28755e1ff5f1cc
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-02-27 11:12:40 -0300

    Update aula057

--- Exercicios/aula057/aula057.js
+++ Exercicios/aula057/aula057.js
@@ -1,11 +1,26 @@
 const Pessoa = {
-    nome:"Guilherme"
+    nome,
+    idade,
+    getNome:function(){
+        return this.nome
+    },
+    getIdade:function(){
+        return this.idade
+    },
+    setNome:function(nome){
+        return this.nome=nome
+    },
+    setIdade:function(idade){
+        return this.idade=idade
+    }
 }
 
 const p2 = Pessoa
 const p3 = Pessoa
 
-p3.nome = "Guizin"
+p3.nome = "GuilhermeP"
+p2["nome"]="GuilhermeM"
+Pessoa.setNome("GuilhermePM")
 
 console.log(Pessoa.nome)
 console.log(p2.nome)
