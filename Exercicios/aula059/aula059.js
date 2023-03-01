51cdd2edf2a7975b229888520f57b03b4cefca2f
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-03-01 11:06:13 -0300

    Update aula059

--- Exercicios/aula059/aula059.js
+++ Exercicios/aula059/aula059.js
@@ -5,18 +5,23 @@ const f_blindagem = document.querySelector("#f_blindagem")
 const f_municao = document.querySelector("#f_municao")
 const f_nome = document.querySelector("#f_nome")
 const f_portas = document.querySelector("#f_portas")
-const carros = document.querySelector("#f_carros")
+const carros = document.querySelector("#carros")
 const btn_addCarro = document.querySelector("#btn_addCarro")
-
 
 let a_carros=[]
 
 f_tipoMilitar.addEventListener("click",(evt)=>{
+    f_blindagem.value=0
+    f_municao.value=0
+    f_nome.value=""
+    f_portas.value=0
     f_blindagem.removeAttribute("disabled")
     f_municao.removeAttribute("disabled")
 })
 
 f_tipoNormal.addEventListener("click",(evt)=>{
+    f_nome.value=""
+    f_portas.value=0
     f_blindagem.value=0
     f_municao.value=0
     f_blindagem.setAttribute("disabled","disabled")
@@ -28,14 +33,22 @@ const exibicaoCarros=()=>{
     a_carros.forEach((c)=>{
         const div=document.createElement("div")
         div.setAttribute("class","carro")
-        div.innerHTML=c.nome
+        div.innerHTML=`Nome: ${c.nome}<br/>`
+        div.innerHTML+=`Portas: ${c.portas}<br/>`
+        div.innerHTML+=`Blindagem: ${c.blindagem}<br/>`
+        div.innerHTML+=`Munição: ${c.municao}<br/>`
+        div.innerHTML+=`Cor: ${c.cor}<br/>`
         carros.appendChild(div)
     })
+      
 }
 
 btn_addCarro.addEventListener("click",()=>{
     if(f_tipoNormal.checked){
         const c=new Carro(f_nome.value,f_portas.value)
+        a_carros.push(c)
+    }else{
+        const c=new Militar(f_nome.value,f_portas.value,f_blindagem.value,f_municao.value)
         a_carros.push(c)
     }
     exibicaoCarros()
@@ -47,7 +60,7 @@ class Carro {//Classe PAI
         this.portas=portas
         this.ligado=false
         this.vel=0
-        this.cor=undefined
+        this.cor="Preto"
     }
 
     Ligar=function(){
