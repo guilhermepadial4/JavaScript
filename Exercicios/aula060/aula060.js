ab312999238cd123142a3d568766b399c743ffe9
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-03-02 11:07:13 -0300

    Update aula060

---
+++ Exercicios/aula060/aula060.js
@@ -0,0 +1,17 @@
+const pessoa={
+    nome:"Guilherme",
+    idade: 19,
+    curso:"Javascript",
+    aulas:{
+        modulo01:"Introdução",
+        modulo02:"Variaveis",
+        modulo03:"Condicional"
+    }
+}
+
+const s_json=JSON.stringify(pessoa)
+const o_json=JSON.parse(s_json)
+
+console.log(pessoa)
+console.log(s_json)
+console.log(o_json)
