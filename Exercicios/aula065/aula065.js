ed529bd529e7f8d1955261ad2160e250f02566c2
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-03-09 09:32:33 -0300

    Update aula065

--- Exercicios/aula065/aula065.js
+++ Exercicios/aula065/aula065.js
@@ -6,18 +6,44 @@ const display=document.querySelector(".display")
 const tecla_On=document.querySelector("#ton")
 const tecla_Limpar=document.querySelector("#tlimpar")
 
+let sinal=false
+let decimal=false
+
 teclas_Num.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+        sinal=false
+        if(display.innerHTML=="0"){
+            display.innerHTML=""
+        }
+        if(evt.target.innerHTML==","){
+            if(!decimal){
+                decimal=true
+                display.innerHTML+=evt.target.innerHTML
+            }
+        }else{
+            display.innerHTML+=evt.target.innerHTML
+        }
     })
 })
 
 teclas_Op.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML+=evt.target.innerHTML
+        if(!sinal){
+            sinal=true
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+            if(evt.target.innerHTML=="x"){
+                display.innerHTML+="*"
+            }else{
+            display.innerHTML+=evt.target.innerHTML
+            }
+        }
     })
 })
 
 tecla_Limpar.addEventListener("click",()=>{
+    sinal=false
+    decimal=false
     display.innerHTML="0"
 })
