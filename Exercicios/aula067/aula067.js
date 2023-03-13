abbfe5778d71f5d124e61db5690e7a7ee26a884c
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-03-13 11:55:32 -0300

    Update aula067

--- Exercicios/aula067/aula067.js
+++ Exercicios/aula067/aula067.js
@@ -1,12 +1,27 @@
 const div_data=document.querySelector("#div_data")
-const data=new Date
+const div_relogio=document.querySelector("#div_relogio")
+
+const data=new Date()
 
 let dia=data.getDate()
 dia=dia<10?"0"+dia:dia
-
 let mes=data.getMonth()
 mes=mes<10?"0"+mes:mes
-
 const data_r=dia+"/"+mes+"/"+data.getFullYear()
-
 div_data.innerHTML=data_r
+
+const relogio=()=>{
+    const data=new Date()
+    let horas=data.getHours()
+    horas=horas<10?"0"+horas:horas
+    let minutos=data.getMinutes()
+    minutos=minutos<10?"0"+minutos:minutos
+    let segundos=data.getSeconds()
+    segundos=segundos<10?"0"+segundos:segundos
+    const hora_completa=horas+":"+minutos+":"+segundos
+    div_relogio.innerHTML=hora_completa
+}
+
+const intervalo=setInterval(relogio,1000)
+
+relogio()
