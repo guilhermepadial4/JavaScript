9084d794ae4f9aeb861609c93db93908f8711c74
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-03-17 10:12:14 -0300

    Update aula070

---
+++ Exercicios/aula070/aula070.js
@@ -0,0 +1,11 @@
+
+const palco=document.querySelector("#palco")
+const num_objetos=document.querySelector("#num_objetos")
+const txt_qtde=document.querySelector("#txt_qtde")
+const btn_add=document.querySelector("#btn_add")
+const btn_remover=document.querySelector("#btn_remover")
+
+let largura_palco=palco.offsetWidth
+let altura_palco=palco.offsetHeight
+let bolas=[]
+let num_bola=0
