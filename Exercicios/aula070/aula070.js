ea24fcc371b24962d56fc674bbda9d1f1550d7bc
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-03-20 09:28:12 -0300

    Update aula070

--- Exercicios/aula070/aula070.js
+++ Exercicios/aula070/aula070.js
@@ -10,6 +10,18 @@ let altura_palco=palco.offsetHeight
 let bolas=[]
 let num_bola=0
 
+class Bola{
+     constructor(){
+          this.tam=Math.floor(Math.random()*15)+10
+          this.r=Math.floor(Math.random()*255)
+          this.g=Math.floor(Math.random()*255)
+          this.b=Math.floor(Math.random()*255)
+          this.px=Math.floor(Math.random()*(largura_palco-this.tam))
+          this.px=Math.floor(Math.random()*(altura_palco-this.tam))
+     }
+
+}
+
 window.addEventListener("resize",(evt)=>{
      largura_palco=palco.offsetWidth
      altura_palco=palco.offsetHeight
@@ -17,7 +29,14 @@ window.addEventListener("resize",(evt)=>{
 })
 
 btn_add.addEventListener("click",(evt)=>{
+     const qtde=txt_qtde.value
+     for(let i=0;i<qtde;i++){
+
+     }
 })
 
 btn_remover.addEventListener("click",(evt)=>{
+     bolas.map((b)=>{
+
+     })
 })
