742dee65d91ec635ada6b9ad1a310d00cbe90bd2
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-03-22 10:12:23 -0300

    Update aula071

---
+++ Exercicios/aula071/aula071.js
@@ -0,0 +1,16 @@
+
+const url=document.querySelector("#url")
+const btn_url=document.querySelector("#btn_url")
+
+btn_url.addEventListener("click",(evt)=>{
+    //window.location="https://www.google.com.br"
+    //window.location.replace("https://www.google.com.br")//Deleta a URL corrente do histórico
+    //window.location.assign("https://www.google.com.br")//NÃO deleta a URL corrente do histórico
+    //window.location.reload()
+    //window.history.back()
+    //window.history.forward()
+    //window.history.go(1)
+    //console.log(window.history.length)
+    //console.log(url.value)
+    window.location=url.value
+})
