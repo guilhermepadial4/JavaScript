abd47dce7613484ec6aa419d9b84c5da17392db4
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-03-28 09:29:09 -0300

    Update aula076

---
+++ Exercicios/aula076/aula076.js
@@ -0,0 +1,8 @@
+//import GetTodosCursos,{cursos} from "./cursos.js"
+
+import getTodosCursos from "./cursos2.js";
+import {cursos,getCursos} from "./cursos2.js"
+
+console.log(cursos)
+console.log(getTodosCursos())
+console.log(getCursos(0))
