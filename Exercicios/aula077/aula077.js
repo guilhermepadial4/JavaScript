1dee57f9e1d801d80d6c9116cce1c3b76177b9d2
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-03-28 09:50:20 -0300

    Update aula077

--- Exercicios/aula077/aula077.js
+++ Exercicios/aula077/aula077.js
@@ -1,8 +1,10 @@
-//import GetTodosCursos,{cursos} from "./cursos.js"
+//import getTodosCursos,{cursos} from "./cursos3.js"
 
-import getTodosCursos from "./cursos3.js";
-import {cursos,getCursos} from "./cursos3.js"
+import getTodosCursos from "./cursos3.js"
+//import {cursos as c,getCursos as gc} from "./cursos3.js"
+import * as m_cursos from "./cursos3.js"
 
-console.log(cursos)
-console.log(getTodosCursos())
-console.log(getCursos(0))
+console.log(m_cursos.cursos)
+ console.log(m_cursos.getCursos(1))
+ console.log(m_cursos.default())
+ 
