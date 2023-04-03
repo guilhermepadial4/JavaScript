59cc5e13d5965e58aaa69fa4f06741c0c756854e
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-03 08:53:59 -0300

    Update aula083

---
+++ Exercicios/aula083/aula083.js
@@ -0,0 +1,19 @@
+let nome=new String("Guilherme")
+let curso=new String("Javascript")
+
+console.log(nome)
+console.log(typeof(nome))
+
+console.log(nome.charAt(0))
+
+console.log(nome.charCodeAt(0))
+
+console.log(nome.concat(curso))
+
+//nome=nome.concat(curso)
+
+console.log(nome)
+
+console.log(nome.indexOf("e"))
+
+console.log(nome.lastIndexOf("r"))
