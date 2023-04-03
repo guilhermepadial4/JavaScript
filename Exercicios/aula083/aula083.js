2cc97dd404093cfa001fc7a61d9a8732639493a1
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-03 09:23:20 -0300

    Update aula083

--- Exercicios/aula083/aula083.js
+++ Exercicios/aula083/aula083.js
@@ -1,4 +1,5 @@
-let nome=new String("Guilherme")
+let nome=new String("Guilherme Padial")
+let nome2=new String("Guilherme Padial")
 let curso=new String("Javascript")
 
 console.log(nome)
@@ -16,4 +17,16 @@ console.log(nome)
 
 console.log(nome.indexOf("e"))
 
-console.log(nome.lastIndexOf("r"))
+console.log(nome.lastIndexOf("e"))
+
+console.log(nome.localeCompare(nome2))
+
+console.log(nome.replace("G","K"))
+
+console.log(nome.search("Padial"))
+
+let sobrenome=nome.slice(10,16)
+console.log(sobrenome)
+
+let arr_nome=nome.split(" ")
+console.log(arr_nome)
