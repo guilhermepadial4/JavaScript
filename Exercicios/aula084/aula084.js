75abb4f7caf0e3297d9393f47bd112533f5025e6
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-03 10:11:33 -0300

    Update aula084

---
+++ Exercicios/aula084/aula084.js
@@ -0,0 +1,17 @@
+let nome=new String("Guilherme Padial")
+
+console.log(nome.startsWith("G"))
+
+console.log(nome.endsWith("l"))
+
+console.log(nome.includes("e"))
+
+console.log(nome.repeat(4))
+
+console.log(nome.charCodeAt(0))
+
+console.log(String.fromCharCode(71))
+
+let nome_ts="Guilherme"
+
+console.log(`Nome: ${nome_ts}`)
