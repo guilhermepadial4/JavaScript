27467a0b4cc9a3d69c657c360b0aa51caedcb536
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-04 09:49:52 -0300

    Update aula085

--- Exercicios/aula085/aula085.js
+++ Exercicios/aula085/aula085.js
@@ -1,4 +1,7 @@
-let nome=new String("Guilherme Padial")
+let nome=new String("Guilherme Padial 2012")
+let email="123@teste.com"
+let numeros="1,10,100,1000"
+
 console.log(nome)
 
 console.log(nome.search(/padial/i))
@@ -6,3 +9,15 @@ console.log(nome.search(/padial/i))
 console.log(nome.match(/l/g))
 
 console.log(nome.replace(/l/ig,"Teste"))
+
+// Metacaracteres
+console.log(nome.match(/\d/ig)) //Números
+console.log(nome.match(/\s/ig)) //Espaços
+console.log(nome.match(/\b/ig)) //DWORD
+
+//Quantificadores
+console.log(nome.match(/i+|l+/ig))
+console.log(numeros.match(/10/ig))
+console.log(numeros.match(/10+/ig))
+console.log(numeros.match(/10*/ig))
+console.log(numeros.match(/10?/ig))
