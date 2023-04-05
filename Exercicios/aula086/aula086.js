f5d4824b5fd1d0813c5cc74c55d7888410480387
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-05 10:55:30 -0300

    Update aula086

--- Exercicios/aula086/aula086.js
+++ Exercicios/aula086/aula086.js
@@ -6,15 +6,34 @@ const btn_parar=document.querySelector("#btn_parar")
 const initi=()=>{
     carro.style.position="relative"
     carro.style.left="0px"
+    carro.style.width="100px"
+    tamMax=window.innerWidth - parseInt(carro.style.width)
 }
 
 let anima=null
 
+let tamMax=null
+
 const mover=(dir)=>{
-    let posi=parseInt(carro.style.left)
-    posi+=(10*dir)
-    carro.style.left=`${posi}px`
-    anima=setTimeout(mover,20,dir)
+    if(dir > 0){
+        if(parseInt(carro.style.left) <= tamMax){
+            let posi=parseInt(carro.style.left)
+             posi+=(10*dir)
+            carro.style.left=`${posi}px`
+             anima=setTimeout(mover,20,dir)
+        }else{
+            clearTimeout(anima)
+        }
+    }else if(dir < 0){
+        if(parseInt(carro.style.left) >= 0){
+            let posi=parseInt(carro.style.left)
+             posi+=(10*dir)
+            carro.style.left=`${posi}px`
+             anima=setTimeout(mover,20,dir)
+        }else{
+            clearTimeout(anima)
+        }
+    }
 }
 
 btn_parar.addEventListener("click",()=>{
@@ -32,5 +51,8 @@ btn_direita.addEventListener("click",()=>{
     mover(1)
 })
 
-window.onload=initi()
-//window.addEventListener("load",initi())
+//window.onload=initi()
+window.addEventListener("load",initi())
+window.addEventListener("resize",()=>{
+    tamMax=window.innerWidth - parseInt(carro.style.width)
+})
