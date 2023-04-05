f9d186f1d4c3ff679ef6bd338d651e1b3efb4ea9
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-05 09:10:04 -0300

    Update aula086

--- Exercicios/aula086/aula086.js
+++ Exercicios/aula086/aula086.js
@@ -1,3 +1,25 @@
 const carro=document.querySelector("#carro")
 const btn_esquerda=document.querySelector("#btn_esquerda")
 const btn_direita=document.querySelector("#btn_direita")
+
+const initi=()=>{
+    carro.style.position="relative"
+    carro.style.left="0px"
+}
+
+btn_esquerda.addEventListener("click",()=>{
+    //carro.style.left=parseInt(carro.style.left) -10 +"px"
+    let posi=parseInt(carro.style.left)
+    posi+=-10
+    carro.style.left=`${posi}px`
+})
+
+btn_direita.addEventListener("click",()=>{
+    let pos=parseInt(carro.style.left)
+    pos+=10
+    carro.style.left=`${pos}px`
+    console.log(pos)
+})
+
+window.onload=initi()
+//window.addEventListener("load",initi())
