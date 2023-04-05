60ceb18a049eb52848706546e52a696358a95c0f
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-05 10:01:48 -0300

    Update aula086

--- Exercicios/aula086/aula086.js
+++ Exercicios/aula086/aula086.js
@@ -1,24 +1,35 @@
 const carro=document.querySelector("#carro")
 const btn_esquerda=document.querySelector("#btn_esquerda")
 const btn_direita=document.querySelector("#btn_direita")
+const btn_parar=document.querySelector("#btn_parar")
 
 const initi=()=>{
     carro.style.position="relative"
     carro.style.left="0px"
 }
 
+let anima=null
+
+const mover=(dir)=>{
+    let posi=parseInt(carro.style.left)
+    posi+=(10*dir)
+    carro.style.left=`${posi}px`
+}
+
+btn_parar.addEventListener("click",()=>{
+    clearInterval(anima)
+})
+
 btn_esquerda.addEventListener("click",()=>{
     //carro.style.left=parseInt(carro.style.left) -10 +"px"
-    let posi=parseInt(carro.style.left)
-    posi+=-10
-    carro.style.left=`${posi}px`
+    clearInterval(anima)
+    anima=setInterval(mover,20,-1)
+    
 })
 
 btn_direita.addEventListener("click",()=>{
-    let pos=parseInt(carro.style.left)
-    pos+=10
-    carro.style.left=`${pos}px`
-    console.log(pos)
+    clearInterval(anima)
+    anima=setInterval(mover,20,1)
 })
 
 window.onload=initi()
