3b5587d737541c1c7b87a2b901e223ff3c8bdbd6
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-06 10:23:09 -0300

    Update aula086

--- Exercicios/aula086/aula086.js
+++ Exercicios/aula086/aula086.js
@@ -1,58 +1,54 @@
 const carro=document.querySelector("#carro")
-const btn_esquerda=document.querySelector("#btn_esquerda")
-const btn_direita=document.querySelector("#btn_direita")
 const btn_parar=document.querySelector("#btn_parar")
+const btn_rodar=document.querySelector("#btn_rodar")
 
 const initi=()=>{
     carro.style.position="relative"
     carro.style.left="0px"
     carro.style.width="100px"
-    tamMax=window.innerWidth - parseInt(carro.style.width)
+    carro.style.height="40px"
+    tamCarro=parseInt(carro.style.width)
+    tamMax=window.innerWidth - tamCarro
 }
 
 let anima=null
-
 let tamMax=null
+let tamCarro=null
+let dir=0
 
-const mover=(dir)=>{
-    if(dir > 0){
-        if(parseInt(carro.style.left) <= tamMax){
-            let posi=parseInt(carro.style.left)
-             posi+=(10*dir)
-            carro.style.left=`${posi}px`
-             anima=setTimeout(mover,20,dir)
-        }else{
-            clearTimeout(anima)
-        }
-    }else if(dir < 0){
-        if(parseInt(carro.style.left) >= 0){
-            let posi=parseInt(carro.style.left)
-             posi+=(10*dir)
-            carro.style.left=`${posi}px`
-             anima=setTimeout(mover,20,dir)
-        }else{
-            clearTimeout(anima)
-        }
+const mover=()=>{
+    if(parseInt(carro.style.left) >= tamMax){
+        dir=-1
+    }else if(parseInt(carro.style.left) <= 0){
+        dir=1
     }
+    carro.style.left=parseInt(carro.style.left)+(dir*10)+"px"
+    anima=setTimeout(mover,20)
 }
 
 btn_parar.addEventListener("click",()=>{
     clearTimeout(anima)
 })
 
-btn_esquerda.addEventListener("click",()=>{
-    //carro.style.left=parseInt(carro.style.left) -10 +"px"
-    clearTimeout(anima)
-    mover(-1)
-})
-
-btn_direita.addEventListener("click",()=>{
-    clearTimeout(anima)
-    mover(1)
+btn_rodar.addEventListener("click",()=>{
+    mover()
 })
 
 //window.onload=initi()
 window.addEventListener("load",initi())
 window.addEventListener("resize",()=>{
     tamMax=window.innerWidth - parseInt(carro.style.width)
+})
+
+window.addEventListener("keydown",(evt)=>{
+    if(evt.code==="ArrowUp"){
+        carro.style.width=parseInt(carro.style.width)+10+"px"
+        carro.style.height=parseInt(carro.style.width)+10+"px"
+    }
+    if(evt.code==="ArrowDown"){
+        carro.style.width=parseInt(carro.style.width)-10+"px"
+        carro.style.height=parseInt(carro.style.width)-10+"px"
+    }
+    tamCarro=parseInt(carro.style.width)
+    tamMax=window.innerWidth - tamCarro
 })
