5f0e9313c8c9129e3adbc2fa250689bd8fd067d9
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-11 09:23:51 -0300

    Update aula090

---
+++ Exercicios/aula090/aula090.js
@@ -0,0 +1,29 @@
+let numeros=[10,20,30,40,50,60,70,80,90]
+let[a,b,c,...d]=numeros
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+
+let obj={nome:"Guilherme",curso:"Javascript"}
+let {nome,curso}=obj
+
+console.log(nome)
+console.log(curso)
+
+const cores=()=>{
+    return ["roxo","azul","amarelo","preto"]
+}
+
+let [c1,c2,,c3]=cores()
+
+console.log(c1)
+console.log(c2)
+console.log(c3)
+
+let texto="Curso de Javascript"
+let [...t]=texto.split(" ")
+
+console.log(t)
