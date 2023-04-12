cbfaa6d69c1386a10a52223e5948786961159e4c
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-12 09:42:25 -0300

    Update aula092

---
+++ Exercicios/aula092/aula092.js
@@ -0,0 +1,35 @@
+// let i=null; i=0
+// console.log(i)
+
+// let i=null
+// for(i=0;i<10;i++);
+// console.log(i)
+
+
+// let curso="Javascript";
+// ['J','a','v','a'].forEach(l=>{console.log(l)})
+
+// let n1=[1,2]
+// let n2=[3,4];
+// [n1,n2].map(v=>console.log(v))
+
+
+// let n=null;
+// (n=10)
+// console.log(n)
+
+// let numeros=[10,20,30];
+// [n1,n2,n3]=numeros
+// console.log(n1)
+// console.log(n2)
+// console.log(n3)
+
+// let num={
+//     n1:10,
+//     n2:20,
+//     n3:30
+// }
+// let {n1,n2,n3}=num
+// console.log(n1)
+// console.log(n2)
+// console.log(n3)
