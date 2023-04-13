59da9f6d4f160fdb9254b23290ceb75ebe24a01e
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-13 10:03:56 -0300

    Update aula093

--- Exercicios/aula093/aula093.js
+++ Exercicios/aula093/aula093.js
@@ -2,13 +2,18 @@ const f_nome=document.querySelector("#f_nome")
 const f_nota=document.querySelector("#f_nota")
 const f_msg=document.querySelector("#f_msg")
 
-document.querySelector("btn_validar").addEventListener("click",(evt)=>{
-    let msg=null
+document.querySelector("#btn_validar").addEventListener("click",(evt)=>{
+    let estadoValidacao=f_nota.validity
 
-    if(!f_nota.checkValidity()){
-        msg=f_nota.validationMessage
+    if(estadoValidacao.valueMissing){
+        f_nota.setCustomValidity("Este campo é obrigatório")
+    }else if(estadoValidacao.rangeOverFlow){
+        f_nota.setCustomValidity("Nota inválida, digite uma nota de 0 a 10")
+    }else if(estadoValidacao.rangeUnderflow){
+        f_nota.setCustomValidity("Nota inválida, digite uma nota de 0 a 10")
     }
 
-    f_msg.innerHTML=msg
+    f_nota.reportValidity()
+    //f_msg.innerHTML=f_nota.validationMessage()
     evt.preventDefault()
 })
