0e85b67ca8857aad8f7f9967ca2e9f0e4f548c25
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-17 10:32:58 -0300

    Update aula095

--- Exercicios/aula095/aula095.js
+++ Exercicios/aula095/aula095.js
@@ -1,6 +1,17 @@
-const endpoint = "https://lustroustametriggers.guilhermepadial.repl.co"
-fetch(endpoint)
-.then(res=>res.json())
-.then(dados=>{
-    console.log(dados)
-})
+const p_temp=document.getElementById("p_temp")
+const p_nivel=document.getElementById("p_nivel")
+const p_press=document.getElementById("p_press")
+
+const obterDados=()=>{
+    const endpoint = "https://lustroustametriggers.guilhermepadial.repl.co"
+    fetch(endpoint)
+    .then(res=>res.json())
+    .then(dados=>{
+        console.log(dados)
+        p_temp.innerHTML="Temperatura: " + dados.temperatura
+        p_nivel.innerHTML="Nivel: " + dados.nivel
+        p_press.innerHTML="Pressão: " + dados.pressão
+    })
+}
+
+let intervalo=setInterval(obterDados,5000)
