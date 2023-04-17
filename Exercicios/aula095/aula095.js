80a32a4c39a20e93c7426791882a171a703235ef
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-17 11:23:47 -0300

    Update aula095

--- Exercicios/aula095/aula095.js
+++ Exercicios/aula095/aula095.js
@@ -1,6 +1,7 @@
 const p_temp=document.getElementById("p_temp")
 const p_nivel=document.getElementById("p_nivel")
 const p_press=document.getElementById("p_press")
+const btn_texto=document.getElementById("btn_texto")
 
 const obterDados=()=>{
     const endpoint = "https://lustroustametriggers.guilhermepadial.repl.co"
@@ -14,4 +15,27 @@ const obterDados=()=>{
     })
 }
 
-let intervalo=setInterval(obterDados,5000)
+// let intervalo=setInterval(obterDados,5000)
+
+let dados={
+    nome:"Guilherme",
+    curso:"Javascript"
+}
+
+let cabecalho={
+    method:"POST",
+    body:JSON.stringify(dados)
+}
+
+const gravarDados=()=>{
+    const endpoint="https://lustroustametriggers.guilhermepadial.repl.co"
+    fetch(endpoint,cabecalho)
+    .then(res=>res.json())
+    .then(ret=>{
+        console.log(ret)
+    })
+}
+
+btn_texto.addEventListener("click",(evt)=>{
+    gravarDados()
+})
