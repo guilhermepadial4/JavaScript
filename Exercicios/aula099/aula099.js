deed02c8a6abcc1f192394be79c60a0d9b6c2207
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-20 09:39:07 -0300

    Update aula099

--- Exercicios/aula099/aula099.js
+++ Exercicios/aula099/aula099.js
@@ -11,8 +11,12 @@ const contador=()=>{
 
 const converter=(seg)=>{
     const hora=Math.floor(seg/3600)
+    const resto=seg%3600
+    const minuto=Math.floor(resto/60)
+    const segundo=Math.floor(resto%60)
+    const form=(hora<10?"0"+hora:hora)+":"+(minuto<10?"0"+minuto:minuto)+":"+(segundo<10?"0"+segundo:segundo)
 
-    return  hora
+    return form
 }
 
 setInterval(contador,1000)
