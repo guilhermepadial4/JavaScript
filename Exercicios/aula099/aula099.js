2a6e3ad75d4267a45bffec1928db616737b6b0ad
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-20 10:54:45 -0300

    Update aula099

--- Exercicios/aula099/aula099.js
+++ Exercicios/aula099/aula099.js
@@ -1,6 +1,10 @@
 const timer=document.getElementById("timer")
+const btn_iniciar=document.getElementById("btn_iniciar")
+const btn_parar=document.getElementById("btn_parar")
+const btn_zerar=document.getElementById("btn_zerar")
 
-const tmpini=Date.now()
+let intervalo=null
+let tmpini=null
 
 const contador=()=>{
     const tmpatual=Date.now()
@@ -14,9 +18,22 @@ const converter=(seg)=>{
     const resto=seg%3600
     const minuto=Math.floor(resto/60)
     const segundo=Math.floor(resto%60)
-    const form=(hora<10?"0"+hora:hora)+":"+(minuto<10?"0"+minuto:minuto)+":"+(segundo<10?"0"+segundo:segundo)
+    const tempoformatado=(hora<10?"0"+hora:hora)+":"+(minuto<10?"0"+minuto:minuto)+":"+(segundo<10?"0"+segundo:segundo)
 
-    return form
+    return tempoformatado
 }
 
-setInterval(contador,1000)
+btn_iniciar.addEventListener("click",(evt)=>{
+    tmpini=Date.now()
+    intervalo=setInterval(contador,1000)
+})
+
+btn_parar.addEventListener("click",(evt)=>{
+    clearInterval(intervalo)
+})
+
+btn_zerar.addEventListener("click",(evt)=>{
+    tmpini=Date.now()
+    timer.innerHTML="00:00:00"
+    clearInterval(intervalo)
+})
