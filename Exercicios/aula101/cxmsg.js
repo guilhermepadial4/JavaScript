5eee7b0f031fb535342c10af3b82db4aa5c6004f
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-25 11:19:17 -0300

    Update aula101

--- Exercicios/aula101/cxmsg.js
+++ Exercicios/aula101/cxmsg.js
@@ -12,6 +12,45 @@ class Cxmsg{
     }
     mostrar=()=>{
         this.divmsg=document.createElement("div")
+        const estilo_divmsg=
+            "display: flex;"+
+            "justify-content: center;"+
+            "align-items: center;"+
+            "position: absolute;"+
+            "top: 0px;"+
+            "left: 0px;"+
+            "width: 100%;"+
+            "height: 100vh;"+
+            "background-color: rgba(0,0,0,0.7);"
+        this.divmsg.setAttribute("id","divmsg")
+        this.divmsg.setAttribute("style",estilo_divmsg)
+        this.destino.prepend(this.divmsg)
+
+        const estilo_areaCxmsg=
+            "display: flex;"+
+            "justify-content: flex-start;"+
+            "align-items: flex-start;"+
+            "flex-direction: column;"+
+            "width: 300px;"
+
+        const areaCxmsg=document.createElement("div")
+        areaCxmsg.setAttribute("style",estilo_areaCxmsg)
+        this.divmsg.appendChild(areaCxmsg)
+
+
+        const estilo_tituloCxmsg=
+            "display: flex;"+
+            "justify-content: flex-start;"+
+            "align-items: center;"+
+            "width: 100%;"+
+            "background-color:"+this.cor+";"+
+            "color:#fff"+
+            "padding:5px"+
+            "border-radius:5px 5px 0px 0px"
+        const tituloCxmsg=document.createElement("div")
+        tituloCxmsg.setAttribute("style",estilo_tituloCxmsg)
+        tituloCxmsg.innerHTML=this.titulo
+        areaCxmsg.appendChild(tituloCxmsg)
     }
     ocultar=()=>{
 
