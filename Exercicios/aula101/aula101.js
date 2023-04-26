d2bf2a67500675193c48e13609e33b8147668e46
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-26 09:53:07 -0300

    Update aula101

--- Exercicios/aula101/aula101.js
+++ Exercicios/aula101/aula101.js
@@ -1,12 +1,20 @@
 const config={
-    titulo:"Teste",
-    texto:"Curso de Javascript",
     cor:"#42a"
 }
 const cxmsg=new Cxmsg(config)
 
 const btn_mostrarcxmsg=document.getElementById("btn_mostrarcxmsg")
+const btn_mostrarcxmsg2=document.getElementById("btn_mostrarcxmsg2")
+const btn_mostrarcxmsg3=document.getElementById("btn_mostrarcxmsg3")
 
 btn_mostrarcxmsg.addEventListener("click",()=>{
-    cxmsg.mostrar()
+    cxmsg.mostrar("Aluno","Guilherme Padial")
+})
+
+btn_mostrarcxmsg2.addEventListener("click",()=>{
+    cxmsg.mostrar("Curso","Javascript")
+})
+
+btn_mostrarcxmsg3.addEventListener("click",()=>{
+    cxmsg.mostrar("Matricula","25647822")
 })
