5c00acc82060eaf0cfd08958cde7455eede71fd4
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-04-27 10:13:12 -0300

    Update aula101

--- Exercicios/aula101/aula101.js
+++ Exercicios/aula101/aula101.js
@@ -1,22 +1,35 @@
 import { Cxmsg } from "./cxmsg.js"
 
 const config={
-    cor:"#48f"
+    cor:"#48f",
+    tipo:"ok",
+    textos:["SIM","NÃO"],
+    comando_sn:()=>{}
+
  }
-Cxmsg.config(config)
+
+ const fsim=()=>{
+    console.log("Botão SIM Pressionado")
+ }
 
 const btn_mostrarcxmsg=document.getElementById("btn_mostrarcxmsg")
 const btn_mostrarcxmsg2=document.getElementById("btn_mostrarcxmsg2")
 const btn_mostrarcxmsg3=document.getElementById("btn_mostrarcxmsg3")
 
 btn_mostrarcxmsg.addEventListener("click",()=>{
-    Cxmsg.mostrar("Aluno","Guilherme Padial")
+    config.tipo="ok"
+    Cxmsg.mostrar(config,"Aluno","Guilherme Padial")
 })
 
 btn_mostrarcxmsg2.addEventListener("click",()=>{
-    Cxmsg.mostrar("Curso","Javascript")
+    config.tipo="sn"
+    config.comando_sn=()=>{fsim()}
+    Cxmsg.mostrar(config,"Curso","Javascript")
 })
 
 btn_mostrarcxmsg3.addEventListener("click",()=>{
-    Cxmsg.mostrar("Matricula","25647822")
+    config.tipo="sn"
+    config.textos=["OK","CANCELA"]
+    config.comando_sn=()=>{}
+    Cxmsg.mostrar(config,"Matricula","25647822")
 })
--- Exercicios/aula101/cxmsg.js
+++ Exercicios/aula101/cxmsg.js
@@ -2,13 +2,15 @@ class Cxmsg{
    static cor="#888"
    static destino=null
    static divmsg=null
+   static tipo=null
+   static comando_sn=null
+   static textos=[]
 
-   static config=(config)=>{
+    static mostrar=(config,titulo,texto)=>{
         this.cor=config.cor
-        
-    }
-
-    static mostrar=(titulo,texto)=>{
+        this.tipo=config.tipo
+        this.textos=config.textos
+        this.comando_sn=()=>{config.comando_sn()}
         this.destino=document.body
         this.titulo=titulo
         this.texto=texto
@@ -90,13 +92,33 @@ class Cxmsg{
         "cursor:pointer;"+
         "text-transform:uppercase;"
 
-        const btn_ok=document.createElement("button")
-        btn_ok.setAttribute("style",estilo_botaoCxmsg)
-        btn_ok.innerHTML="OK"
-        btn_ok.addEventListener("click",(evt)=>{
-            this.ocultar()
-        })
-        rodapeCxmsg.appendChild(btn_ok)
+        if(this.tipo=="ok"){
+            const btn_ok=document.createElement("button")
+            btn_ok.setAttribute("style",estilo_botaoCxmsg)
+            btn_ok.innerHTML="OK"
+            btn_ok.addEventListener("click",(evt)=>{
+                this.ocultar()
+            })
+            rodapeCxmsg.appendChild(btn_ok)
+
+        }else if(this.tipo=="sn"){
+            const btn_sim=document.createElement("button")
+            btn_sim.setAttribute("style",estilo_botaoCxmsg)
+            btn_sim.innerHTML=this.textos[0]
+            btn_sim.addEventListener("click",(evt)=>{
+                this.comando_sn()
+                this.ocultar()
+            })
+            rodapeCxmsg.appendChild(btn_sim)
+
+            const btn_nao=document.createElement("button")
+            btn_nao.setAttribute("style",estilo_botaoCxmsg)
+            btn_nao.innerHTML=this.textos[1]
+            btn_nao.addEventListener("click",(evt)=>{
+                this.ocultar()
+            })
+            rodapeCxmsg.appendChild(btn_nao)
+        }
     }
 
    static ocultar=()=>{
