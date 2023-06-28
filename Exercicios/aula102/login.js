0019c9a932260b713760221499fcd3a717605edb
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-06-28 12:14:25 -0300

    Update aula102

--- Exercicios/aula102/login.js
+++ Exercicios/aula102/login.js
@@ -4,9 +4,18 @@ class Login{
     static nomelogado = null;
     static stylecss = null
     static acessologado = null;
+    static config = {
+        cor: "#048",
+        img: "./logo.png"
+    };
     static endpoint = "https://loginv1padialdev.guilhermepadial.repl.co/";
     //https://loginv1padialdev.guilhermepadial.repl.co/?matricula=123&senha=221
-    static login = (mat,pas) => {
+
+    static login = (mat,pas,config=null) => {
+        if(config != null){
+            this.config = config;
+        }
+
          this.endpoint += `?matricula=${mat}&senha=${pas}`;
 
          this.stylecss =
@@ -90,7 +99,7 @@ class Login{
         
         '.buttons-login button{'+
             'cursor: pointer;'+
-            'background-color: #048;'+
+            `background-color: ${this.config.cor};`+
             'color: #fff;'+
             'border-radius: 5px;'+
             'padding: 10px;'+
@@ -104,6 +113,112 @@ class Login{
         linkStyle.setAttribute("type", "text/css");
         linkStyle.innerHTML = this.stylecss;
         document.head.appendChild(linkStyle);
+
+        const body = document.body;
+
+        const backLogin = document.createElement("div");
+        backLogin.setAttribute("id", "back-login");
+        backLogin.setAttribute("class", "back-login"); 
+        document.body.prepend(backLogin); 
+
+        const baseLogin = document.createElement("div");
+        baseLogin.setAttribute("id", "base-login");
+        baseLogin.setAttribute("class", "base-login"); 
+        backLogin.appendChild(baseLogin); 
+
+        const elementsLogin = document.createElement("div");
+        elementsLogin.setAttribute("id", "elements-login");
+        elementsLogin.setAttribute("class", "elements-login"); 
+        baseLogin.appendChild(elementsLogin); 
+
+        const campLoginUsername = document.createElement("div");
+        campLoginUsername.setAttribute("id", "campLoginUsername");
+        campLoginUsername.setAttribute("class", "camp-login"); 
+        elementsLogin.appendChild(campLoginUsername); 
+
+        const LabelUsername = document.createElement("label");
+        LabelUsername.setAttribute("id", "LabelUsername");
+        LabelUsername.innerHTML = "Usuário"
+        campLoginUsername.appendChild(LabelUsername); 
+
+        const inputUsername = document.createElement("input");
+        inputUsername.setAttribute("id", "inputUsername");
+        inputUsername.setAttribute("type", "text")
+        inputUsername.setAttribute("name", "f_username")
+        campLoginUsername.appendChild(inputUsername); 
+
+        const campLoginPassword = document.createElement("div");
+        campLoginPassword.setAttribute("id", "campLoginPassword");
+        campLoginPassword.setAttribute("class", "camp-login"); 
+        elementsLogin.appendChild(campLoginPassword); 
+
+        const LabelPassword = document.createElement("label");
+        LabelPassword.setAttribute("id", "LabelUsername");
+        LabelPassword.innerHTML = "Senha"
+        campLoginPassword.appendChild(LabelPassword); 
+
+        const inputPassword = document.createElement("input");
+        inputPassword.setAttribute("id", "inputPassword");
+        inputPassword.setAttribute("type", "password")
+        inputPassword.setAttribute("name", "f_password")
+        campLoginPassword.appendChild(inputPassword); 
+        
+        const ButtonsLogin = document.createElement("div");
+        ButtonsLogin.setAttribute("id", "ButtonsLogin");
+        ButtonsLogin.setAttribute("class", "buttons-login"); 
+        elementsLogin.appendChild(ButtonsLogin); 
+
+        const btnLogin = document.createElement("button");
+        btnLogin.setAttribute("id", "btn-login");
+        btnLogin.innerHTML = "Login"
+        ButtonsLogin.appendChild(btnLogin); 
+
+        const btnCancel = document.createElement("button");
+        btnCancel.setAttribute("id", "btn-cancel");
+        btnCancel.innerHTML = "Cancel"
+        ButtonsLogin.appendChild(btnCancel); 
+
+        const soonLogin = document.createElement("div");
+        soonLogin.setAttribute("id", "soon-login");
+        soonLogin.setAttribute("class", "soon-login"); 
+        baseLogin.appendChild(soonLogin); 
+
+        const imgLogin = document.createElement("img");
+        imgLogin.setAttribute("src", "./logo.png");
+        imgLogin.setAttribute("alt", "padialdev");
+        soonLogin.appendChild(imgLogin); 
+
+
+    //     <div id="back-login" class="back-login"> OK
+
+    //     <div id="base-login" class="base-login">OK
+
+    //             <div id="elements-login" class="elements-login">OK
+
+    //                 <div class="camp-login">OK
+    //                     <label>Username</label>OK
+    //                     <input type="text" name="f_username" id="f-username">ok
+    //                 </div>
+
+    //                 <div class="camp-login">ok
+    //                     <label>Senha</label>ok
+    //                     <input type="password" name="f_password" id="f-password">ok
+    //                 </div>
+
+    //                 <div class="buttons-login">OK
+    //                     <button id="btn-login">Login</button>OK
+    //                     <button id="btn-cancel">Cancelar</button>OK
+    //                 </div>
+
+    //             </div>
+
+    //             <div id="soon-login" class="soon-login">OK
+    //                 <img src="./logo.png" alt="Padialdev">OK
+    //             </div>
+
+    //      </div>
+    // </div> 
+    
 
         // fetch(this.endpoint)
         // .then(res=>res.json())
